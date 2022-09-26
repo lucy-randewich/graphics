a387e9b4039802c238af38eb665cee1db816a68e
laurcty <lrandewich@gmail.com>
2022-09-26 20:18:47 +0100

    added interpolation function

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -7,6 +7,8 @@
 #define WIDTH 320
 #define HEIGHT 240
 
+using namespace std;
+
 void draw(DrawingWindow &window) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
@@ -32,9 +34,25 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 	}
 }
 
+vector<float> interpolateSingleFloats(float from, float to, int numberOfValue) {
+    vector<float> vec;
+    float increment = (to-from)/(numberOfValue-1);
+    for(size_t i = 0; i < numberOfValue; i++){
+        vec.push_back(from);
+        from += increment;
+    }
+    return vec;
+}
+
 int main(int argc, char *argv[]) {
-	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+    std::vector<float> result;
+    result = interpolateSingleFloats(2.2, 8.5, 7);
+    for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+    std::cout << std::endl;
+
+    DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
@@ -42,4 +60,5 @@ int main(int argc, char *argv[]) {
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
+
 }
